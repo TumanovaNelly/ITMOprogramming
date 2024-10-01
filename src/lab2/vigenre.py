30684bc00e02056515dff2e127989be00cc9d763
TumanovaNelly <2-man.off@mail.ru>
2024-10-01 22:45:08 +0300

    ref. vigenere

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -1,3 +1,4 @@
+NUM_LETTERS = ord('z') - ord('a') + 1
 """
 Encrypts plaintext using a Vigenere cipher.
 >>> encrypt_vigenere("PYTHON", "A")
@@ -9,18 +10,17 @@ Encrypts plaintext using a Vigenere cipher.
 """
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     ciphertext = []
-    num_letters = ord('z') - ord('a') + 1
     for i in range(len(plaintext)):
-        if plaintext[i] >= 'a' and plaintext[i] <= 'z':
-            new_ord = ord(plaintext[i]) + ord(keyword[i % len(keyword)]) - ord('a')
-            ciphertext.append(chr(new_ord - (num_letters if new_ord > ord('z') else 0)))
-        elif plaintext[i] >= 'A' and plaintext[i] <= 'Z':
-            new_ord = ord(plaintext[i]) + ord(keyword[i % len(keyword)]) - ord('A')
-            ciphertext.append(chr(new_ord - (num_letters if new_ord > ord('Z') else 0)))
-        else:
-            ciphertext.append(plaintext[i])
+        new_ord = ord(plaintext[i])
+        if 'a' <= plaintext[i] <= 'z':
+            new_ord = ord('a') + (new_ord + ord(keyword[i % len(keyword)].lower()) - ord('a') * 2) % NUM_LETTERS
+        elif 'A' <= plaintext[i] <= 'Z':
+            new_ord = ord('A') + (new_ord + ord(keyword[i % len(keyword)].upper()) - ord('A') * 2) % NUM_LETTERS
+
+        ciphertext.append(chr(new_ord))
 
     return "".join(ciphertext)
+
 print(encrypt_vigenere("PYTHON", "A"))
 print(encrypt_vigenere("python", "a"))
 print(encrypt_vigenere("ATTACKATDAWN", "LEMON"))
@@ -36,16 +36,14 @@ Decrypts a ciphertext using a Vigenere cipher.
 """
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     plaintext = []
-    num_letters = ord('z') - ord('a') + 1
     for i in range(len(ciphertext)):
-        if ciphertext[i] >= 'a' and ciphertext[i] <= 'z':
-            new_ord = ord(ciphertext[i]) - ord(keyword[i % len(keyword)]) + ord('a')
-            plaintext.append(chr(new_ord + (num_letters if new_ord < ord('a') else 0)))
-        elif ciphertext[i] >= 'A' and ciphertext[i] <= 'Z':
-            new_ord = ord(ciphertext[i]) - ord(keyword[i % len(keyword)]) + ord('A')
-            plaintext.append(chr(new_ord + (num_letters if new_ord < ord('A') else 0)))
-        else:
-            plaintext.append(ciphertext[i])
+        new_ord = ord(ciphertext[i])
+        if 'a' <= ciphertext[i] <= 'z':
+            new_ord = ord('a') + (new_ord - ord(keyword[i % len(keyword)].lower())) % NUM_LETTERS
+        elif 'A' <= ciphertext[i] <= 'Z':
+            new_ord = ord('A') + (new_ord - ord(keyword[i % len(keyword)].upper())) % NUM_LETTERS
+
+        plaintext.append(chr(new_ord))
     return "".join(plaintext)
 
 print(decrypt_vigenere("PYTHON", "A"))
